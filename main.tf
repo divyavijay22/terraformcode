f6903dd9f741b03e6b776eac3708f1b0c6250554
divyavijay22 <121885562+divyavijay22@users.noreply.github.com>
2023-09-14 02:03:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-0f5ee92e2d63afc18"  # Specify your desired AMI ID
-  instance_type = "t2.micro"              # Choose an appropriate instance type
+  instance_type = "t3.medium"              # Choose an appropriate instance type
 
   subnet_id = "subnet-087e34ae78c5eeab1"  # Specify your existing subnet ID
   vpc_security_group_ids = ["sg-0be4bd6a1bb04b689"]  # Specify your existing security group IDs
