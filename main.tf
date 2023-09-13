72444bce7761bb56753a76e18b0bad7dda8b727e
tamilarasan027 <138055486+tamilarasan027@users.noreply.github.com>
2023-09-13 18:39:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "example" {
   subnet_id = "subnet-0b63dd819549fdf3e"  # Specify your existing subnet ID
   vpc_security_group_ids = ["sg-0af37425b60efb46d"]  # Specify your existing security group IDs
 
-  key_name = "promotheustamil.pem"        # Specify the name of your SSH key pair
+  key_name = "my-key-pair"        # Specify the name of your SSH key pair
 
   tags = {
     Name = "ExampleInstance"
