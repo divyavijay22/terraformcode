a3253d53c171219ee1dd4b0219cfeb54ce6caf96
divyavijay22 <121885562+divyavijay22@users.noreply.github.com>
2023-09-18 15:28:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,17 +7,17 @@ terraform {
   }
 }
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0f5ee92e2d63afc18"  # Specify your desired AMI ID
+resource "aws_instance" "Test-ec2" {
+  ami           = "ami-04cb4ca688797756f"  # Specify your desired AMI ID
   instance_type = "t3.medium"              # Choose an appropriate instance type
 
-  subnet_id = "subnet-087e34ae78c5eeab1"  # Specify your existing subnet ID
-  vpc_security_group_ids = ["sg-0be4bd6a1bb04b689"]  # Specify your existing security group IDs
+  subnet_id = "subnet-0c476a42644c28b3c"  # Specify your existing subnet ID
+  vpc_security_group_ids = ["sg-0ec6baec404a4de1b"]  # Specify your existing security group IDs
 
-  key_name = "terranew"        # Specify the name of your SSH key pair
+  key_name = "securends-non-prod"        # Specify the name of your SSH key pair
 
   tags = {
     Name = "TerraformEC2Instance"
